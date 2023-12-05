05b3d9e977ec0408cbb893fb189ba85c1a0a6d7e
Ahmed Mohamed <am0103738@gmail.com>
2023-12-05 09:59:06 +0200

    Documented project details.

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Guess-My-Number
+
+This project is a web application developed as part of [The Complete JavaScript Course 2024: From Zero to Expert!](https://www.udemy.com/course/the-complete-javascript-course/).
+
+## Description
+
+The Guess-My-Number website allows users to play a number guessing game. The user is prompted to enter a number within a specified range, and the application will provide feedback on whether the guess is too high or too low. The goal is to guess the correct number within the fewest attempts possible.
+
+## Features
+
+- User-friendly interface
+- Randomly generated target number
+- Feedback on each guess
+- Score tracking to keep record of the user's performance
+
+## Getting Started
+
+To get started with the Guess-My-Number website, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Open the `index.html` file in your web browser.
+3. Start playing the game by entering your guesses and following the prompts.
+
+## What I Learned and What I Did
+
+In this project, I learned how to create a number guessing game using JavaScript. I used the following concepts and techniques:
+
+- Generating a random number within a specified range using the `generateRandomNumber` function.
+- Handling user input and validating it to ensure it is a valid number within the specified range.
+- Providing feedback to the user based on their guess, such as whether the guess is too high or too low.
+- Keeping track of the number of attempts remaining and updating the score accordingly.
+- Updating the high score if the user guesses the correct number within the fewest attempts.
+- Resetting the game and generating a new random number when the user chooses to play again.
+
+This project helped me practice my JavaScript skills and understand how to manipulate the DOM to create interactive web applications.
+
+## Screenshot
+
+![Guess-My-Number Screenshot](./screenshot.gif)
+
+## Live Demo
+
+You can try out the live demo of the Guess-My-Number website [here](https://ahmedmohamedabdelaty.github.io/Guess-My-Number/).
+
+## Acknowledgements
+
+- This project was developed as part of [The Complete JavaScript Course 2024: From Zero to Expert!](https://www.udemy.com/course/the-complete-javascript-course/) course.
+- Special thanks to the course instructor for providing guidance and resources.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 let secretNumber = generateRandomNumber(1, 20);
 let attemptsRem = 20;
@@ -10,49 +10,49 @@ const changeColor = function (className, color) {
   document.querySelector(`.${className}`).style.color = color;
 };
 
-document.querySelector('.check-btn').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.in').value);
+document.querySelector(".check-btn").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".in").value);
 
   if (!guess && attemptsRem > 0) {
-    displayMessage('Enter a valid number! 😕', '#C75941');
+    displayMessage("Enter a valid number! 😕", "#C75941");
   } else if ((guess <= 0 || guess > 20) && attemptsRem > 0) {
-    displayMessage('Please enter a number from 1 to 20 only! 😕', '#C75941');
+    displayMessage("Please enter a number from 1 to 20 only! 😕", "#C75941");
   } else if (guess === secretNumber && attemptsRem > 0) {
-    document.querySelector('.q').textContent = secretNumber;
-    displayMessage('🎉 Correct Number!', '#2C57AC');
-    document.querySelector(`body`).style.backgroundColor = '#59C06A';
-    document.querySelector(`.box`).style.transition = 'width 0.5s'; // Add transition property
-    document.querySelector(`.box`).style.width = '200px';
+    document.querySelector(".q").textContent = secretNumber;
+    displayMessage("🎉 Correct Number!", "#2C57AC");
+    document.querySelector(`body`).style.backgroundColor = "#59C06A";
+    document.querySelector(`.box`).style.transition = "width 0.5s"; // Add transition property
+    document.querySelector(`.box`).style.width = "200px";
     updateHighScore();
   } else {
     const difference = Math.abs(guess - secretNumber);
-    let message = '';
+    let message = "";
 
     if (difference > 5) {
-      message = 'Too ';
+      message = "Too ";
     }
 
     if (guess > secretNumber) {
-      message += 'high! 📈';
+      message += "high! 📈";
     } else {
-      message += 'low! 📉';
+      message += "low! 📉";
     }
 
     attemptsRem--;
 
     if (attemptsRem <= 0) {
       if (attemptsRem === 0) {
-        document.querySelector('.score-num').textContent = attemptsRem;
+        document.querySelector(".score-num").textContent = attemptsRem;
       }
-      displayMessage('You lost! Try again 😢', '#DB522E');
+      displayMessage("You lost! Try again 😢", "#DB522E");
     } else {
-      document.querySelector('.score-num').textContent = attemptsRem;
-      displayMessage(message, '#C75941');
+      document.querySelector(".score-num").textContent = attemptsRem;
+      displayMessage(message, "#C75941");
     }
   }
 });
 
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector(".again").addEventListener("click", function () {
   resetGame();
 });
 
@@ -61,25 +61,25 @@ function generateRandomNumber(min, max) {
 }
 
 function displayMessage(message, color) {
-  document.querySelector('.message').textContent = message;
-  changeColor('message', color);
+  document.querySelector(".message").textContent = message;
+  changeColor("message", color);
 }
 
 function updateHighScore() {
   highScore = Math.max(highScore, attemptsRem);
-  document.querySelector('.hscore-num').textContent = highScore;
+  document.querySelector(".hscore-num").textContent = highScore;
 }
 
 function resetGame() {
   secretNumber = generateRandomNumber(1, 20);
   console.log(secretNumber);
   attemptsRem = 20;
-  document.querySelector('.score-num').textContent = 20;
-  document.querySelector('.q').textContent = '?';
-  document.querySelector('.message').textContent = 'Start guessing...';
-  changeColor('q', '#222');
-  changeColor('message', '#eeeeee');
-  document.querySelector('.in').value = '';
-  document.querySelector(`body`).style.backgroundColor = '#222';
-  document.querySelector(`.box`).style.width = '120px';
+  document.querySelector(".score-num").textContent = 20;
+  document.querySelector(".q").textContent = "?";
+  document.querySelector(".message").textContent = "Start guessing...";
+  changeColor("q", "#222");
+  changeColor("message", "#eeeeee");
+  document.querySelector(".in").value = "";
+  document.querySelector(`body`).style.backgroundColor = "#222";
+  document.querySelector(`.box`).style.width = "120px";
 }
